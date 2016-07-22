dfec5febc368d06388018901883e0a7de77fc10a
verzhinskaya <verzhinskayasite@gmail.com>
2016-07-22 22:15:21 +0200

    New main site version

---
+++ js/carousel_init.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+	  autoPlay : 7000,
+	  pagination : false,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
